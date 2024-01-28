ad6c95f93929b99960af946686946bcc98457bdb
Johnson Masino <johnsonmasino@gmail.com>
2024-01-28 07:35:15 +0100

    Created Querries to get all chats for a user

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from user.models import User, Chat
+from .serializers import ChatSerializer
+from rest_framework.generics import ListAPIView
+from django.db.models import Q, Subquery, OuterRef
 
-# Create your views here.
+# This is for the chat view
+class MyChatView(ListAPIView):
+    serializer_class = ChatSerializer
+    
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+    
+        messages = Chat.objects.filter(
+            id__in=Subquery(
+                User.objects.filter(
+                    Q(sender__receiver=user_id),
+                    Q(receiver__sender=user_id)
+                ).distinct().annotate(
+                    last_message_id=Subquery(
+                        Chat.objects.filter(
+                            Q(sender=OuterRef('id'), receiver=user_id),
+                            Q(receiver=OuterRef('id'), sender=user_id)
+                        ).order_by('-id')[:1].values_list('id', flat=True)
+                    )
+                ).values_list('last_message_id', flat=True).order_by('-id')
+            )
+        ).order_by('-id')
+        return messages
